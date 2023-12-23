4c57544c00b4f0bc09e564bc6a29289446c5cb28
Khanggggggggg <100785988+Khanggggggggg@users.noreply.github.com>
2023-12-23 22:21:55 +0700

    .

--- QLPKNK_App/QLPKNK_App/AddMedicine.cs
+++ QLPKNK_App/QLPKNK_App/AddMedicine.cs
@@ -1,22 +1,12 @@
-﻿using QLPKNK_App.BUS;
-using QLPKNK_App.DTO;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
-using System.Net;
-using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
-using System.Xml.Linq;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
-using System.Configuration;
 
 namespace QLPKNK_App
 {
